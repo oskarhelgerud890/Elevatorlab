0b64e4550df6cc6ef122acb4c445325b35b1e262
Øydis Herland <oydishe.stud.ntnu.no>
2022-02-21 17:00:12 +0100

    implement state-enum and set up skeleton for FSM-switch
    Signed-off-by: Øydis Herland <oydishe.stud.ntnu.no>

--- skeleton_project/source/FSM.c
+++ skeleton_project/source/FSM.c
@@ -1,1 +1,22 @@
 #include "FSM.h"
+/**
+ * @brief FSM.c
+ * 
+ */
+
+void FSMSwitch(State currentState){
+    switch (currentState)
+    {
+    case IDLE:
+        break;
+    
+    case MOVING:
+        break;
+    
+    case EMERGENCY:
+        break;
+    
+    default:
+        break;
+    }
+}
--- skeleton_project/source/FSM.h
+++ skeleton_project/source/FSM.h
@@ -1,10 +1,26 @@
+/**
+ * @brief FSM module. states,cases for each state 
+ * 
+ */
 #ifndef FSM__H
 #define FSM_H
 
 #include "elevator.h"
 
+/**
+ * @brief Enum for the different possible states of the elevator.
+ */
+typedef enum{
+    IDLE = 0,
+    MOVING = 1,
+    EMERGENCY = 2,
+}State;
 
-
+/**
+ * @brief switch containing each case/state of the elevator
+ * 
+ */
+void FSMSwitch(State currentState);
 
 
 #endif // FSM_H
