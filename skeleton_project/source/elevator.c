ed8a9004d479d644b40dc3e767f99c57ba530d55
Øydis Herland <oydishe.stud.ntnu.no>
2022-02-21 16:50:29 +0100

    Implement Wrapper functions for elev_io
    Signed-off-by: Øydis Herland <oydishe.stud.ntnu.no>

--- skeleton_project/source/elevator.c
+++ skeleton_project/source/elevator.c
@@ -1,4 +1,7 @@
 #include "elevator.h"
+#include <time.h>
+
+
 
 
 //Elev_io functins
@@ -7,8 +10,84 @@ void setElevatorDirection(MotorDirection dir) {
     return;
 }
 
-void setButtonLamp(int floor, ButtonType button, int value);
-void setFloorLamp(int floor);
-void setDoorOpenLamp(int value);
-void setStopButton(int value);
+void setButtonLamp(int floor, ButtonType button, int value){
+    elevio_buttonLamp(floor, button, value);
+    return;
+}
 
+void setFloorLamp(int floor){
+    elevio_floorIndicator(floor);
+    return;
+}
+
+void setDoorOpenLamp(int value){
+    elevio_doorOpenLamp(value);
+    return;
+}
+
+void setStopButton(int value){
+    elevio_stopButtonLamp(value);
+    return;
+}
+
+int getOrderButtons(int floor, ButtonType button){
+    return elevio_callButton(floor, button);  
+}
+
+int getFloor(void){
+    return elevio_floorSensor();
+}
+
+int getStopButton(void){
+    return elevio_stopButton();
+}
+
+int getObstructionButton(void){
+    return elevio_obstruction();
+}
+
+
+
+
+
+
+
+//Init-function
+void elevatorInit(Elevator* p_elevator){
+    elevio_motorDirection(DIRN_UP);
+
+    while(p_elevator->currentFloor!=-1){
+        int floor = elevio_floorSensor();
+        printf("floor: %d \n",floor);
+
+        if(floor == 0){
+            elevio_motorDirection(DIRN_UP);
+        }
+
+        if(floor == N_FLOORS-1){
+            elevio_motorDirection(DIRN_DOWN);
+        }
+
+
+        for(int f = 0; f < N_FLOORS; f++){
+            for(int b = 0; b < N_BUTTONS; b++){
+                int btnPressed = elevio_callButton(f, b);
+                elevio_buttonLamp(f, b, btnPressed);
+            }
+        }
+
+        if(elevio_obstruction()){
+            elevio_stopLamp(1);
+        } else {
+            elevio_stopLamp(0);
+        }
+        
+        if(elevio_stopButton()){
+            elevio_motorDirection(DIRN_STOP);
+            break;
+        }
+        
+        nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
+    }
+ 
+}
--- skeleton_project/source/elevator.h
+++ skeleton_project/source/elevator.h
@@ -22,20 +22,80 @@ typedef struct
 
 }Elevator;
 
-void elevatorInit(void);
+void elevatorInit(Elevator* p_elevator);
 
 /**
-* @brief Set elevator direction. Wrapper function for
+* @brief Wrapper functions for
 * the elevio library.
 */
 
-
-//Elev_io functins
+/**
+ * @brief Set the elevator direction
+ * 
+ * @param dir 
+ */
 void setElevatorDirection(MotorDirection dir);
+
+/**
+ * @brief Set a choosen button lamp
+ * 
+ * @param floor 
+ * @param button 
+ * @param value 
+ */
 void setButtonLamp(int floor, ButtonType button, int value);
+
+/**
+ * @brief Set a choosen floor lamp
+ * 
+ * @param floor 
+ */
 void setFloorLamp(int floor);
+
+/**
+ * @brief Set the DoorOpen lamp
+ * 
+ * @param value 
+ */
 void setDoorOpenLamp(int value);
+
+/**
+ * @brief Set the stop-button lamp
+ * 
+ * @param value 
+ */
 void setStopButton(int value);
+
+/**
+ * @brief Get the value of a choosen button
+ * 
+ * @param floor 
+ * @param button 
+ * @return value of button, int
+ */
+int getOrderButtons(int floor, ButtonType button);
+
+/**
+ * @brief Get the current floor
+ * 
+ * @return Current floor, int
+ */
+int getFloor(void);
+
+/**
+ * @brief Get the value of stop button
+ * 
+ * @return Stop button vlaue, int 
+ */
+int getStopButton(void);
+
+/**
+ * @brief Get the value of the obstructon button
+ * 
+ * @return Obstruction button value, int 
+ */
+int getObstructionButton(void);
+
 
 //Button Functions
 void updateOrders(Elevator *p_elevator);
