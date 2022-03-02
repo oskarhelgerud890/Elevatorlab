611dfe102054ea4169b98276ae52cc7c8625f0fa
ronja kræmer <rkhansen01@gmail.com>
2022-03-02 11:48:16 +0100

    Pragma once stuff and defines in header
    Signed-off-by: ronja kræmer <rkhansen01@gmail.com>

--- skeleton_project/source/timer.h
+++ skeleton_project/source/timer.h
@@ -1,9 +1,14 @@
-//pragma once stuff
-// include stuff
-// defines
+#ifndef TIMER_H
+#define TIMER_H
+
+
+#define TIME_OUT 1
+#define TIME_NOT_OUT 0
 
 void setTimer();
 
 int timeOut();
 
+
+#endif // TIMER_H
 
