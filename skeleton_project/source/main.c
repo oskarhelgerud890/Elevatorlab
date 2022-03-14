edd2c3bd5c774478b0707e9649549d07000b2489
ronja kræmer <rkhansen01@gmail.com>
2022-03-14 14:41:57 +0100

    make branch for clean up and commenting
    Signed-off-by: ronja kræmer <rkhansen01@gmail.com>

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -34,41 +34,4 @@ int main(){
     return 0;
 }
 
-/*
-printf("=== Example Program ===\n");
-    printf("Press the stop button on the elevator panel to exit\n");
-
-    elevio_motorDirection(DIRN_UP);
-
-    while(1){
-        int floor = elevio_floorSensor();
-        printf("floor: %d \n",floor);while(1)
-            elevio_motorDirection(DIRN_UP);
-        }
-
-        if(floor == N_FLOORS-1){
-            elevio_motorDirection(DIRN_DOWN);
-        }
-
-
-        for(int f = 0; f < N_FLOORS; f++){
-            for(int b = 0; b < N_BUTTONS; b++){
-                int btnPressed = elevio_callButton(f, b);
-                elevio_buttonLamp(f, b, btnPressed);
-            }
-        }
-
-        if(elevio_obstruction()){
-            elevio_stopLamp(1);
-        } else {
-            elevio_stopLamp(0);
-        }
-        
-        if(elevio_stopButton()){
-            elevio_motorDirection(DIRN_STOP);
-            break;
-        }
-        
-        nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
-    }*/
     
